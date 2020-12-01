fcffc897b1e9c69b67ef4b5383d7a1a57cc5d68c
artemZholus <artem.zholus@gmail.com>
2020-12-01 16:54:04 +0300

    upload code

---
+++ training.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+
+import os
+import random
+
+from tqdm import tqdm
+from nltk.tokenize.casual import TweetTokenizer
+from typing import List, Union, Optional
+from scipy.sparse import issparse, isspmatrix, spmatrix
+from time import time
+import pickle
+
+from bpemb import BPEmb
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+from sklearn.model_selection import cross_validate, StratifiedShuffleSplit
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
+from xgboost import XGBClassifier
+from vowpalwabbit.sklearn_vw import VWClassifier
+from catboost import CatBoostClassifier
+from argparse import ArgumentParser
+
+from wrappers import Embedder, BPETokenizer, Lemmatizer, Stemmer, TokenizerWrapper, FeaturePipeline, OnlineSVM
+
+
+df = pd.read_csv('train-balanced-sarcasm.csv', usecols=['comment', 'parent_comment', 'label'])
+df.dropna(axis='index', how='any', inplace=True)
+test_txt = df.comment.iloc[2]
+y = df.label.values
+
+
+preprocessing_configurations = [
+    # tokenizer, [postprocessor, ]vectorizer
+    ('bpe', 'bow'),
+    ('bpe', 'tfidf'),
+    ('bpe', 'hashing'),
+    ('base', 'stem', 'bow'),
+    ('base', 'lemma', 'bow'),
+    ('base', 'stem', 'tfidf'),
+    ('base', 'lemma', 'tfidf'),
+    ('base', 'stem', 'hashing'),
+    ('base', 'lemma', 'hashing'),
+    ('base', 'lemma', 'glove_emb'),
+]
+
+
+def optimize_grid(model, params, n_jobs=10, verbose=0):
+    return GridSearchCV(
+        model, param_grid=params,
+        n_jobs=n_jobs, scoring='f1',
+        cv=5, refit=True, verbose=verbose
+    )
+
+
+def calc_metrics(model, X_test, y_test, X_train, y_train):
+    y_pred_test = model.predict(X_test)
+    y_pred_train = model.predict(X_train)
+    metrics = [f1_score, accuracy_score, precision_score, recall_score, roc_auc_score]
+    names = ['f1', 'acc', 'precision', 'recall', 'roc_auc']
+    res = {}
+    for m, n in zip(metrics, names):
+        res[f'train_{n}'] = m(y_train, y_pred_train)
+        res[f'test_{n}'] = m(y_test, y_pred_test)
+    return res
+
+
+result = {}
+agg = {}
+
+parser = ArgumentParser()
+parser.add_argument('--debug', action='store_true', default=False)
+args = parser.parse_args()
+
+frac = 0.001 if args.debug else 0.2
+X_train, X_test, y_train, y_test = train_test_split(df.comment.values, df.label.values, test_size=frac)
+
+if args.debug:
+    X_train = X_test
+    y_train = y_test
+
+
+for preproc_conf in tqdm(preprocessing_configurations, desc='optimization...'):
+    print(f'Examination of configuration: {preproc_conf}')
+    if len(preproc_conf) == 3:
+        tokenizer, postprocessor, vectorizer = preproc_conf
+    else:
+        tokenizer, vectorizer = preproc_conf
+        postprocessor = 'none'
+    t = time()
+    preprocessor = FeaturePipeline(tokenizer=tokenizer,
+                                   postprocessor=postprocessor,
+                                   vectorizer=vectorizer)
+    X_train_features = preprocessor.fit_transform(X_train)
+    X_test_features = preprocessor.transform(X_test)
+    del preprocessor
+    print(f'preprocessing took {time() - t:.4f} seconds')
+
+    t = time()
+    gs = optimize_grid(
+        LogisticRegression(solver='liblinear'),
+        {'C': [.01, .1, .2, .5, .8, 1., 3, 5, 10]},
+        n_jobs=16, verbose=10
+    ).fit(X_train_features, y_train.astype(np.bool))
+    result[(preproc_conf, 'logreg')] = gs.cv_results_
+    agg[(preproc_conf, 'logreg')] = calc_metrics(
+        gs.best_estimator_, X_test_features, y_test.astype(np.bool),
+        X_train_features, y_train.astype(np.bool))
+    agg[(preproc_conf, 'logreg')]['best_params'] = gs.best_params_
+    print(agg[(preproc_conf, 'logreg')])
+    print(f'logreg training took {time() - t:.4f} seconds')
+
+#     t = time()
+#     gs = optimize_grid(
+#         OnlineSVM(),
+#         {'l2': [0.1, 0.5, 1., 3., 5.],
+#          'ksvm': [False, True], 'kernel': ['rbf']},
+#         n_jobs=10, verbose=10
+#     ).fit(X_train_features, y_train.astype(np.bool))
+#     result[(preproc_conf, 'svm')] = gs.cv_results_
+#     agg[(preproc_conf, 'svm')] = calc_metrics(
+#         gs.best_estimator_, X_test_features, y_test.astype(np.bool),
+#         X_train_features, y_train.astype(np.bool))
+#     agg[(preproc_conf, 'svm')]['best_params'] = gs.best_params_
+#     print(f'svm training took {time() - t:.4f} seconds')
+
+    t = time()
+    gs = optimize_grid(
+        XGBClassifier(n_estimators=1000, n_jobs=16),
+        {'max_depth': [3, 5, 10], 'n_estimators': [500, 1000, 2000]}, n_jobs=1,
+        verbose=10
+    ).fit(X_train_features, y_train)
+    result[(preproc_conf, 'xgb')] = gs.cv_results_
+    agg[(preproc_conf, 'xgb')] = calc_metrics(
+        gs.best_estimator_, X_test_features, y_test,
+        X_train_features, y_train)
+    agg[(preproc_conf, 'xgb')]['best_params'] = gs.best_params_
+    print(f'xgb training took {time() - t:.4f} seconds')
+
+    #t = time()
+    #if isinstance(X_train_features, spmatrix):
+    #    X_train_cat = X_train_features.tocsc()
+    #    X_test_cat = X_test_features.tocsc()
+    #else:
+    #    X_train_cat = X_train_features
+    #    X_test_cat = X_test_features
+    #gs = optimize_grid(
+    #    CatBoostClassifier(),
+    #    {'max_depth': [3, 5, 10], 'n_estimators': [500, 1000, 2000]}, n_jobs=1,
+    #    verbose=10
+    #).fit(X_train_cat, y_train)
+    #result[(preproc_conf, 'cat')] = gs.cv_results_
+    #agg[(preproc_conf, 'cat')] = calc_metrics(
+    #    gs.best_estimator_, X_test_cat, y_test,
+    #    X_train_cat, y_train)
+    #agg[(preproc_conf, 'cat')]['best_params'] = gs.best_params_
+    #print(f'cat training took {time() - t:.4f} seconds')
+
+    t = time()
+    gs = optimize_grid(
+        RandomForestClassifier(n_estimators=700, n_jobs=16, verbose=0),
+        {'max_depth': [3, 5, 10], 'n_estimators': [700, 1000, 2000]}, n_jobs=1,
+        verbose=10
+    ).fit(X_train_features, y_train.astype(np.bool))
+    result[(preproc_conf, 'rf')] = gs.cv_results_
+    agg[(preproc_conf, 'rf')] = calc_metrics(
+        gs.best_estimator_, X_test_features, y_test.astype(np.bool),
+        X_train_features, y_train.astype(np.bool))
+    agg[(preproc_conf, 'rf')]['best_params'] = gs.best_params_
+    print(f'rf training took {time() - t:.4f} seconds')
+
+
+results_df = pd.DataFrame.from_records(list(agg.values()))
+results_df['model'] = [k[1] for k in agg.keys()]
+results_df['tokenizer'] = [k[0][0] for k in agg.keys()]
+results_df['postprocessor'] = [(k[0][1] if len(k[0]) == 3 else 'none') for k in agg.keys()]
+results_df['vectorizer'] = [k[0][-1] for k in agg.keys()]
+
+results_df.to_csv('training_results.csv', index=False)
+
+with open('full_results.pkl', 'wb') as f:
+    pickle.dump(result, f)
---
+++ wrappers.py
@@ -0,0 +1,213 @@
+from sklearn.base import TransformerMixin
+import youtokentome as yttm
+import numpy as np
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from tempfile import NamedTemporaryFile
+import gensim.downloader as api
+from nltk.corpus import stopwords
+import nltk
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
+from vowpalwabbit.sklearn_vw import VWClassifier
+from sklearn.base import ClassifierMixin, BaseEstimator
+
+
+class TokenizerWrapper(TransformerMixin):
+    def __init__(self, tokenizer, func='tokenize', lowercase=True, join=False):
+        self.tokenizer = tokenizer
+        self.lowercase = lowercase
+        
+    def fit(self, X, y=None, **kwargs):
+        return self
+    
+    def transform(self, X, y=None, **kwargs):
+        if self.lowercase:
+            for i, text in enumerate((X)):
+                X[i] = text.lower()
+        return [self.tokenizer.tokenize(x) for x in (X)]
+
+class WordPostprocessor(TransformerMixin):
+    def __init__(self, join=False):
+        self.join = join
+        self.word_apply = None
+        
+    def fit(self, X, y=None, **kwargs):
+        return self
+
+    def transform(self, tokenized_texts, y=None, **kwargs):
+        if self.join:
+            return [' '.join(self.word_apply(w) for w in sent) for sent in tokenized_texts]
+        else:
+            return [[self.word_apply(w) for w in sent] for sent in (tokenized_texts)]
+
+class Stemmer(WordPostprocessor):
+    def __init__(self, join=False):
+        super().__init__(join)
+        self.stemmer = PorterStemmer()
+        self.word_apply = self.stemmer.stem
+
+class Lemmatizer(WordPostprocessor):
+    def __init__(self, join=False):
+        super().__init__(join)
+        self.lemmatizer = WordNetLemmatizer()
+        self.word_apply = self.lemmatizer.lemmatize
+    
+class BPETokenizer(TransformerMixin):
+    def __init__(self, vocab_size=1000):
+        self.vocab_size = vocab_size
+        self.bpe = None
+        
+    def fit(self, X, y=None, **kwargs):
+        
+        with NamedTemporaryFile() as f, NamedTemporaryFile() as g:
+            with open(f.name, 'w') as f_desc:
+                for s in X:
+                    f_desc.write(s + '\n')
+            self.bpe = yttm.BPE.train(data=f.name, model=g.name, 
+                                      vocab_size=self.vocab_size)
+        return self
+    
+    def transform(self, X, y=None, **kwargs):
+        if isinstance(X, np.ndarray):
+            X = X.tolist()
+        return self.bpe.encode(X, output_type=yttm.OutputType.SUBWORD)
+
+class Embedder(TransformerMixin):
+    """
+        makes text embedding (as averaged or weighted averaged vector of words)
+    """
+    def __init__(self, embedding):
+        self.vectors = embedding.vectors
+        self.vocab = {k: i for i, k in enumerate(embedding.index2word)}
+        self.embedding = {w: embedding.get_vector(w) for w in embedding.vocab}
+        self.emb_dim = self.embedding[next(iter(self.embedding.keys()))].shape[0]
+
+    def fit(self, X, y=None, **kwargs):
+        return self
+    
+    def transform(self, texts, weights=None):
+        res = []
+        for text in texts:
+            idx = np.array([self.vocab[w] for w in text if w in self.vocab])
+            if len(idx) == 0:
+                res.append(np.zeros(self.vectors.shape[1]))
+            else:
+                res.append(self.vectors[idx].mean(0))
+        return np.row_stack(res)
+
+
+class OnlineSVM(ClassifierMixin, BaseEstimator):
+    def __init__(self, ksvm=False, kernel='linear', l2=1., l1=0.,):
+        self.ksvm = ksvm
+        self.kernel = kernel
+        self.l2 = l2
+        self.l1 = l1
+        self.vw = None
+        
+    def fit(self, X, y):
+        self.vw = VWClassifier(**self.get_params(), loss_function='hinge')
+        self.vw.fit(X, y)
+        return self
+    
+    def predict(self, X):
+        preds = self.vw.predict(X)
+        del self.vw
+        self.vw = None
+        return preds
+
+    
+class FeaturePipeline(TransformerMixin):
+    @staticmethod
+    def ID(x):
+        return x
+
+    def __init__(self, tokenizer='base', postprocessor='lemma', vectorizer='bow',
+                 vectorizer_max_ngram=3, vectorizer_max_df=0.99, vectorizer_min_df=0.0001):
+        self._init(tokenizer=tokenizer, postprocessor=postprocessor, vectorizer=vectorizer, 
+                   vectorizer_max_ngram=vectorizer_max_ngram, vectorizer_max_df=vectorizer_max_df, 
+                   vectorizer_min_df=vectorizer_min_df)
+    
+    def _init(self, tokenizer='base', postprocessor='lemma', vectorizer='bow', 
+              vectorizer_max_ngram=3, vectorizer_max_df=0.99, vectorizer_min_df=0.0001    
+             ):
+        """
+        Args:
+            tokenizer (str): either 'base' or 'bpe'
+            postprocessor (str): either 'lemma', 'stem' or 'none', igroned if tokenizer is bpe
+            vectorizer (str): either 'bow', 'tfidf', 'hashing', 'glove_emb', 'w2v_emb'
+                
+        """
+        self.tokenizer = tokenizer
+        self.postprocessor = postprocessor
+        self.vectorizer = vectorizer
+        self.pipeline = []
+        nltk.download('wordnet')
+        nltk.download('stopwords')
+        self.stopwords = stopwords.words('english')
+        self.vectorizer_max_ngram = vectorizer_max_ngram
+        self.vectorizer_max_df = vectorizer_max_df
+        self.vectorizer_min_df = vectorizer_min_df
+        if tokenizer == 'base':
+            self.pipeline.append(('base_tokenizer', TokenizerWrapper(TweetTokenizer())))
+            if postprocessor == 'stem':
+                self.pipeline.append(('stemmer', Stemmer(
+                   # join=vectorizer == 'bow'
+                )))
+            elif postprocessor == 'lemma':
+                self.pipeline.append(('lemmatizer', Lemmatizer(
+                    #join=vectorizer == 'bow'
+                )))
+            elif postprocessor == 'none':
+                pass
+            else:
+                raise ValueError()
+        elif tokenizer == 'bpe':
+            self.pipeline.append(('bpe', BPETokenizer())) 
+        else:
+            raise ValueError()
+        kwargs = {
+            'ngram_range': (1, self.vectorizer_max_ngram),
+            'analyzer': 'word',
+            'tokenizer': FeaturePipeline.ID
+        }
+        if self.vectorizer == 'bow':
+            name = 'bow'
+            cls = CountVectorizer
+            kwargs['binary'] = True
+            kwargs['max_df'] = self.vectorizer_max_df
+            kwargs['min_df'] = self.vectorizer_min_df
+        elif self.vectorizer == 'tfidf':
+            name = 'tfidf'
+            cls = TfidfVectorizer
+            kwargs['max_df'] = self.vectorizer_max_df
+            kwargs['min_df'] = self.vectorizer_min_df
+        elif self.vectorizer == 'hashing':
+            name = 'hash'
+            cls = HashingVectorizer
+            kwargs['n_features'] = 50000
+        if self.vectorizer in ('bow', 'tfidf', 'hashing'):
+            self.pipeline.append((name, cls(
+                    stop_words=self.stopwords,
+                    lowercase=False, **kwargs
+                )))
+        if self.vectorizer == 'glove_emb':
+            self.pipeline.append((
+                'glove_emb', Embedder(api.load('glove-twitter-50'))
+            ))
+        elif self.vectorizer == 'w2v_emb':
+            self.pipeline.append((
+                'w2v_emb', Embedder(api.load('word2vec-google-news-300'))
+            ))
+        self.pipeline = Pipeline(self.pipeline)
+    
+    def fit_transform(self, X, y=None, **kwargs):
+        return self.pipeline.fit_transform(X, y=y, **kwargs)
+    
+    def transform(self, X, **kwargs):
+        return self.pipeline.transform(X, **kwargs)
+    
+    def set_params(self, **params):
+        super().set_params(**params)
+        self._init(**params)
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
